132dfd0bc3d0c5d6c52133076c2af02fbb4bdf3f
Ashutosh Dash <dashashutosh2007@gmail.com>
2026-02-17 21:21:58 +0530

    UC1

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,17 @@
- public class OOPSBannerApp{
- public static void main(String[] args){
-	 System.out.println("OOPS");
-	 System.out.println("------------------------------------------------------------------------------------------------------");
-	 System.out.println("   *  *      * *     *  *      * *          *  * * * *     ");
-	 System.out.println("  *    *    *   *    *    *    *    *       *              ");
-	 System.out.println(" *     *    *   *    *     *   *     *      *              ");
-	 System.out.println(" *     *    *   *    *    *    *    *       *  *  * *      ");
-	 System.out.println(" *     *    *   *    *  *      *  *                 *      ");
-	 System.out.println("  *   *     *  *     *         *                    *      ");
-	 System.out.println("   * *       * *     *         *            * * * * *      ");
- }
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ *
+ * This class demonstrates a simple Java application that displays the Object
+ * Oriented Programming System OOPS acronym to the console.
+ *
+ * @author Ashutosh
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
 }
