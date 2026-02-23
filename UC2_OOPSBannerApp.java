8cc93a0fe6586d5016bb814a0b59ae5b4bf2e84d
Ashutosh Dash <dashashutosh2007@gmail.com>
2026-02-23 20:26:07 +0530

    added x:

--- UC2_OOPSBannerApp.java
+++ UC2_OOPSBannerApp.java
@@ -3,12 +3,12 @@
  */
 public class UC2_OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println(" *****   *****   ******   ***** ");
-        System.out.println("*     * *     *  *     * *     *");
-        System.out.println("*     * *     *  *     * *      ");
-        System.out.println("*     * *     *  ******   ***** ");
-        System.out.println("*     * *     *  *             *");
-        System.out.println("*     * *     *  *             *");
-        System.out.println(" *****   *****   *        ***** ");
+        System.out.println(" *****   *****   ******   *****  ");
+        System.out.println("*     * *     *  *     * *     * ");
+        System.out.println("*     * *     *  *     * *       ");
+        System.out.println("*     * *     *  ******   *****  ");
+        System.out.println("*     * *     *  *             * ");
+        System.out.println("*     * *     *  *             * ");
+        System.out.println(" *****   *****   *        *****  ");
     }
 }
